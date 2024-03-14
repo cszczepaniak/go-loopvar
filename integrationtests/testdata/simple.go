fc30fa98220fe38bfecffd31fd5e2872213df8f3
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-03-13 20:42:00 -0500

    add to testdata

--- integrationtests/testdata/simple.go
+++ integrationtests/testdata/simple.go
@@ -5,10 +5,17 @@ package testdata
 import "fmt"
 
 func slice() {
+	// simple case
 	for i, v := range []int{1, 2, 3} {
 		i := i
 		v := v
 
 		fmt.Println(i, v)
+	}
+
+	// only thing in the loop
+	for i, v := range []int{1, 2, 3} {
+		i := i
+		v := v
 	}
 }
--- integrationtests/testdata/simple_exp.go
+++ integrationtests/testdata/simple_exp.go
@@ -5,7 +5,12 @@ package testdata
 import "fmt"
 
 func slice() {
+	// simple case
 	for i, v := range []int{1, 2, 3} {
 		fmt.Println(i, v)
+	}
+
+	// only thing in the loop
+	for i, v := range []int{1, 2, 3} {
 	}
 }
